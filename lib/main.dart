ac413d7a3ad6114c0dad00f029b9427485f11174
Haydar Kadıoğlu <95019347+haydarkadioglu@users.noreply.github.com>
2023-06-18 00:54:53 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sirius/pages/homeScreen/Drawer.dart';
 import './pages/bottomNavigator.dart';
 
 import 'package:firebase_core/firebase_core.dart';
